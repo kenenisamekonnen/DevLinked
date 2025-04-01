bd7eb3d89b48e32f80be88d0190fa701b238dc86
Kenenisa Mekonnen <kenenisamekonnensori@gmail.com>
2025-04-01 13:30:16 -0700

    added hash pasword and compare password and hash password

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,2 @@
 import Users from "../models/Users.js";
 
-export const register = async (req, res) => {
-    try{
-        const user = await Users.create(req.body);
-        res.status(200).json(user);
-    } catch(error){
-        console.log("user added to table", error);
-    }
-};
-
