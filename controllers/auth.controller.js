ea21db79290bc2994e2272b28bf2b9a594a965b0
Kenenisa Mekonnen <kenenisamekonnensori@gmail.com>
2025-04-16 11:04:45 -0700

    added authrnticion and get users

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -26,14 +26,24 @@ export const login = async (req, res) => {
     try {
         const {email, password} = req.body;
 
-        const user = await Users.findOne({ where: {email}});
+        const user = await Users.findOne({ where: {email: req.body.email}});
         if(!user) return res.status(404).json({ message: "User not found" });
 
         const isMatch = await comparePassword(password, user.password);
         if(!isMatch) return res.status(400).json({ message: "Invalid credentials"});
 
-        const token = jwt.sign({ id: user.id, role: user.role}, process.env.JWT_SECRET, { expiresIn: '1h'});
-        res.status(200).json({ message: "Login successful", token});
+        // const token = jwt.sign({ id: user.id, role: user.role, email: user.email},
+        //      process.env.JWT_SECRET, 
+        //      { expiresIn: '7d'}
+        // );
+        const token = jwt.sign(
+            { id: user.id, role: user.role, email: user.email }, // ← make sure this line includes email
+            process.env.JWT_SECRET,
+            { expiresIn: '7d' }
+        );
+
+        const { password: _, ...userWithoutPassword } = user.toJSON();
+        res.status(200).json({ message: "Login successful",user: userWithoutPassword, token});
 
     } catch(err) {
         console.error("Login error:", err);
--- routes/userGetRoutes.js
+++ routes/userGetRoutes.js
@@ -1,15 +1,12 @@
 import { Router } from "express";
 import Users from "../models/Users.js";
-import { register, login } from "../controllers/auth.controller.js";
 import { getUser, getUsersById } from "../controllers/users.controller.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = Router();
 
-router.post("/register", register);
-router.post("/login", login);
-router.get("/users", getUser);
-router.get("/user", protect ,getUsersById);
-
+router.get("/getUsers", getUser);
+router.get("/getUsersById", getUsersById);
 
 export default router;
+
